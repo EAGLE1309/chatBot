cab98ec4250519cbe76ffe21bc0628c7e46c5fb6
Eagle <84711967+EAGLE1309@users.noreply.github.com>
2021-11-06 09:25:07 +0530

    Updated Index.js for optimisation

--- index.js
+++ index.js
@@ -2,42 +2,47 @@ const Discord = require('discord.js');
 require('discord-reply');
 require('dotenv').config();
 
-const { activityType, activityName, botChannel } = require('./config.json');
+const {
+    activityType,
+    activityName,
+    botChannel
+} = require('./config.json');
 const fetch = require("node-fetch");
 const client = new Discord.Client();
 
-var colors = require('colors');
+const colors = require('colors');
 
 colors.setTheme({
-  regards: 'cyan',
-  success: 'green',
-  error: 'red',
-  warn: 'yellow'
+    regards: 'cyan',
+    success: 'green',
+    error: 'red',
+    warn: 'yellow'
 }); //for coloured console log
 
 client.login(process.env.Token);
 //process.env for fetching your token in .env file
 
+// you can also replace process.env.Token with "your_bots_token" and it will still work!
 
-client.on("ready", () => { 
-  console.log(`
-████████████████████████████████████████
-█─▄▄▄─█─█─██▀▄─██─▄─▄─█▄─▄─▀█─▄▄─█─▄─▄─█
-█─███▀█─▄─██─▀─████─████─▄─▀█─██─███─███
-▀▄▄▄▄▄▀▄▀▄▀▄▄▀▄▄▀▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▀▀
+client.on("ready", () => {
+    console.log(`
+        ████████████████████████████████████████
+        █─▄▄▄─█─█─██▀▄─██─▄─▄─█▄─▄─▀█─▄▄─█─▄─▄─█
+        █─███▀█─▄─██─▀─████─████─▄─▀█─██─███─███
+        ▀▄▄▄▄▄▀▄▀▄▀▄▄▀▄▄▀▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▀▀
 
 
-`);
-console.log("• Bot is online".success);
-console.log("• Thanks for using this bot!".regards);
+        `);
+    console.log("• Bot is online".success);
+    console.log("• Thanks for using this bot!".regards);
 
-client.user.setPresence({
-    status: "online",
-    activity: {
-        name: activityName, 
-        type: activityType, 
-    }
-  })
+    client.user.setPresence({
+        status: "online",
+        activity: {
+            name: activityName,
+            type: activityType,
+        }
+    })
 });
 
 
@@ -45,19 +50,26 @@ client.user.setPresence({
 //main working (don't do any changes here!)
 
 client.on("message", async message => {
-if (message.channel.name == botChannel) {
-if (message.author.bot) return;
-message.content = message.content.replace(/@(everyone)/gi, "everyone").replace(/@(here)/gi, "here");
-if (message.content.includes(`@`)) {
-return message.channel.send(`**:x: Please dont mention a user noob!!!**`);
- }
-  message.channel.startTyping();
-if (!message.content) return message.channel.send("Please say something.");
-fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${encodeURIComponent(message.content)}&botname=${client.user.username}&ownername=DEVELOPER_NAME`)
-    .then(res => res.json())
-    .then(data => {
-        message.lineReply(`> ${message.content} \n <@${message.author.id}> ${data.message}`);
-    });
-      message.channel.stopTyping();
-}
+    if (message.channel.name == botChannel) {
+
+        if (message.author.bot) return;
+        message.content = message.content.replace(/@(everyone)/gi, "everyone").replace(/@(here)/gi, "here");
+
+        if (message.content.includes(`@`)) {
+            return message.channel.send(`**:x: Please dont mention a user noob!!!**`);
+        }
+
+        message.channel.startTyping();
+
+        if (!message.content) return message.channel.send("Please say something.");
+
+        fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${encodeURIComponent(message.content)}&botname=${client.user.username}&ownername=DEVELOPER_NAME`)
+
+        .then(res => res.json())
+        .then(data => {
+            message.lineReply(`> ${message.content} \n <@${message.author.id}> ${data.message}`);
+        });
+
+        message.channel.stopTyping();
+    }
 });
